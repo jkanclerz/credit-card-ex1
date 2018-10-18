1481b17f1091d8cd7868dca0ba97f1e20fe74331
Jakub Kanclerz <kontakt@jkan.pl>
2018-10-18 09:05:00 +0000

    introduce test happy path for repay

--- creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -14,4 +14,19 @@ public class CreditCardTest {
         Assert.assertTrue(card.getLimit() == 2000);
     }
     
+    @Test
+    public void cantWithdrowWhenNotEnoughFounds() {
+        
+    }
+    
+    @Test
+    public void repayCredit() {
+        CreditCard card = new CreditCard();
+        card.assignLimit(2000);
+        
+        card.withdraw(1000)
+        
+        Assert.assertTrue(card.Saldo() == 1000)
+    }
+    
 }
